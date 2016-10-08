74bc8b4b1fd8c24afe1330f89a78857dea9c8b7d
djmartz77 <djmartz77@yahoo.com>
2016-10-07 21:46:12 -0500

    Added initial design docs
    Added freemind mind-map

---
+++ Design/Stashpocalypse!(knitting and consequences).mm
@@ -0,0 +1,118 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Stashpocalypse!(knitting and consequences)" FOLDED="false" ID="ID_392875548" CREATED="1475882654351" MODIFIED="1475882741968" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Goal" POSITION="right" ID="ID_129285103" CREATED="1475882664126" MODIFIED="1475882768721">
+<edge COLOR="#ff0000"/>
+<node TEXT="Get enough yarn to do your knitting projects" ID="ID_1326052404" CREATED="1475882769791" MODIFIED="1475882779887"/>
+<node TEXT="maybe also do projects" ID="ID_108709993" CREATED="1475882780559" MODIFIED="1475882790761"/>
+</node>
+<node TEXT="Obstacle" POSITION="right" ID="ID_1629550368" CREATED="1475882793740" MODIFIED="1475882797699">
+<edge COLOR="#0000ff"/>
+<node TEXT="Friends keep asking you to do more projects" ID="ID_358616594" CREATED="1475882801379" MODIFIED="1475882832518">
+<node TEXT="More yarn needed" ID="ID_1171444080" CREATED="1475882867274" MODIFIED="1475882873040"/>
+</node>
+<node TEXT="Cat ladies/Other customers" ID="ID_1772234208" CREATED="1475882833102" MODIFIED="1475882851758">
+<node TEXT="Block your access to yarn" ID="ID_1294480258" CREATED="1475882877180" MODIFIED="1475882880882"/>
+</node>
+<node TEXT="Can&apos;t find your yarn" ID="ID_1219036150" CREATED="1475882882652" MODIFIED="1475882908239">
+<node TEXT="You must battle your stash" ID="ID_1358663143" CREATED="1475882910721" MODIFIED="1475882915655">
+<node TEXT="If your stash gets too big it will go on a godzilla-like rampage and destroy the villages" ID="ID_807073007" CREATED="1475883043823" MODIFIED="1475883063937"/>
+</node>
+</node>
+</node>
+<node TEXT="Helpers" POSITION="left" ID="ID_1880783110" CREATED="1475882931928" MODIFIED="1475882957618">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Knitting group" ID="ID_1308297618" CREATED="1475882935784" MODIFIED="1475882944607"/>
+<node TEXT="shopkeepers" ID="ID_865570297" CREATED="1475882945351" MODIFIED="1475882961716"/>
+</node>
+<node TEXT="Overworld RPG (think Final Fantasy. Towns and caves)" POSITION="left" ID="ID_1582001747" CREATED="1475882968087" MODIFIED="1475883003891">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Where to find Yarn" POSITION="right" ID="ID_998153068" CREATED="1475883012708" MODIFIED="1475883015971">
+<edge COLOR="#7c0000"/>
+<node TEXT="Shops" ID="ID_1666554331" CREATED="1475883016561" MODIFIED="1475883021715"/>
+<node TEXT="Caves/Dungeons" ID="ID_965393368" CREATED="1475883022056" MODIFIED="1475883025318"/>
+</node>
+</node>
+</map>
