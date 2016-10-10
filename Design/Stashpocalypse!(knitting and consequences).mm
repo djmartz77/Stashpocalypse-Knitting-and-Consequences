5b1d8ed1b8d51c040c5a0b4e9a188eb8533a997c
djmartz77 <djmartz77@yahoo.com>
2016-10-10 07:58:27 -0500

    More types of games brainstormed

--- Design/Stashpocalypse!(knitting and consequences).mm
+++ Design/Stashpocalypse!(knitting and consequences).mm
@@ -81,14 +81,18 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
-<node TEXT="Goal" POSITION="right" ID="ID_129285103" CREATED="1475882664126" MODIFIED="1475882768721">
-<edge COLOR="#ff0000"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="12" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Overworld RPG (think Final Fantasy or Zelda 2. Towns and caves)" POSITION="left" ID="ID_1582001747" CREATED="1475882968087" MODIFIED="1476100551337">
+<edge COLOR="#00ffff"/>
+<node TEXT="Helpers" ID="ID_1880783110" CREATED="1475882931928" MODIFIED="1475894809624">
+<node TEXT="Knitting group" ID="ID_1308297618" CREATED="1475882935784" MODIFIED="1475882944607"/>
+<node TEXT="shopkeepers" ID="ID_865570297" CREATED="1475882945351" MODIFIED="1475882961716"/>
+</node>
+<node TEXT="Goal" ID="ID_129285103" CREATED="1475882664126" MODIFIED="1475894814333">
 <node TEXT="Get enough yarn to do your knitting projects" ID="ID_1326052404" CREATED="1475882769791" MODIFIED="1475882779887"/>
 <node TEXT="maybe also do projects" ID="ID_108709993" CREATED="1475882780559" MODIFIED="1475882790761"/>
 </node>
-<node TEXT="Obstacle" POSITION="right" ID="ID_1629550368" CREATED="1475882793740" MODIFIED="1475882797699">
-<edge COLOR="#0000ff"/>
+<node TEXT="Obstacle" ID="ID_1629550368" CREATED="1475882793740" MODIFIED="1475894818099">
 <node TEXT="Friends keep asking you to do more projects" ID="ID_358616594" CREATED="1475882801379" MODIFIED="1475882832518">
 <node TEXT="More yarn needed" ID="ID_1171444080" CREATED="1475882867274" MODIFIED="1475882873040"/>
 </node>
@@ -101,18 +105,57 @@
 </node>
 </node>
 </node>
-<node TEXT="Helpers" POSITION="left" ID="ID_1880783110" CREATED="1475882931928" MODIFIED="1475882957618">
-<edge COLOR="#ff00ff"/>
-<node TEXT="Knitting group" ID="ID_1308297618" CREATED="1475882935784" MODIFIED="1475882944607"/>
-<node TEXT="shopkeepers" ID="ID_865570297" CREATED="1475882945351" MODIFIED="1475882961716"/>
-</node>
-<node TEXT="Overworld RPG (think Final Fantasy. Towns and caves)" POSITION="left" ID="ID_1582001747" CREATED="1475882968087" MODIFIED="1475883003891">
-<edge COLOR="#00ffff"/>
-</node>
-<node TEXT="Where to find Yarn" POSITION="right" ID="ID_998153068" CREATED="1475883012708" MODIFIED="1475883015971">
-<edge COLOR="#7c0000"/>
+<node TEXT="Where to find Yarn" ID="ID_998153068" CREATED="1475883012708" MODIFIED="1475894821539">
 <node TEXT="Shops" ID="ID_1666554331" CREATED="1475883016561" MODIFIED="1475883021715"/>
 <node TEXT="Caves/Dungeons" ID="ID_965393368" CREATED="1475883022056" MODIFIED="1475883025318"/>
+</node>
+</node>
+<node TEXT="Action Game" POSITION="right" ID="ID_1081710826" CREATED="1475894835972" MODIFIED="1475894840303">
+<edge COLOR="#00007c"/>
+<node TEXT="You are a ball of yarn" ID="ID_1077857617" CREATED="1475894841487" MODIFIED="1475894846370"/>
+<node TEXT="The stash has grown beyond the knitter&apos;s ability to control it" ID="ID_1382214310" CREATED="1475894846904" MODIFIED="1475894876574"/>
+<node TEXT="You must fight to gain the attention of the knitter to get them to use the stash&apos;s heart for something(Heart made of &quot;Black Heart&quot; yarn?)" ID="ID_1414471834" CREATED="1475894877114" MODIFIED="1476101446307"/>
+<node TEXT="Once this is done, the stash will return to normal, until it grows again." ID="ID_589118405" CREATED="1475894965256" MODIFIED="1475894992769"/>
+</node>
+<node TEXT="vertical shooter(Endless waves mobile)" POSITION="right" ID="ID_11167119" CREATED="1476100554523" MODIFIED="1476100627397">
+<edge COLOR="#007c00"/>
+<node TEXT="You are a ball of yarn" ID="ID_1706635153" CREATED="1476100565609" MODIFIED="1476100569285"/>
+<node TEXT="Instead of shooting, you can entangle" ID="ID_939420942" CREATED="1476100569991" MODIFIED="1476100592680">
+<node TEXT="maybe this is a swipe mechanic?" ID="ID_1980373686" CREATED="1476100642889" MODIFIED="1476100652613"/>
+</node>
+<node TEXT="Dodging cat paws from the crazy cat lady" ID="ID_1594421316" CREATED="1476100593180" MODIFIED="1476100606534"/>
+<node TEXT="Every night there are more cats" ID="ID_1125840672" CREATED="1476100609014" MODIFIED="1476100678665"/>
+<node TEXT="you can allow your yarn to pay out as you roll back and forth, which allows you to lay traps or rewind." ID="ID_1898215990" CREATED="1476101789596" MODIFIED="1476101830039"/>
+</node>
+<node TEXT="endless runner(vertical)" POSITION="left" ID="ID_1676410669" CREATED="1476100712456" MODIFIED="1476100721457">
+<edge COLOR="#7c007c"/>
+<node TEXT="The stash is trying to suck you in! Escape!" ID="ID_1972752635" CREATED="1476100722559" MODIFIED="1476100731616"/>
+<node TEXT="Procedurally generated runner" ID="ID_566888292" CREATED="1476100913298" MODIFIED="1476100921056"/>
+<node TEXT="Main mechanics" ID="ID_142278138" CREATED="1476100923674" MODIFIED="1476100932618">
+<node TEXT="using your yarn to pull you up(grappling hook)" ID="ID_118422385" CREATED="1476100932630" MODIFIED="1476100957440">
+<node TEXT="swipe mechanic" ID="ID_1218886260" CREATED="1476100958118" MODIFIED="1476100961218"/>
+<node TEXT="Can also hang swing" ID="ID_1776083173" CREATED="1476101129726" MODIFIED="1476101138034"/>
+</node>
+<node TEXT="Rolling" ID="ID_259411286" CREATED="1476100975977" MODIFIED="1476101014018">
+<node TEXT="you can allow your yarn to pay out as you roll back and forth, which allows you to lay traps or rewind." ID="ID_1925844834" CREATED="1476101789596" MODIFIED="1476101830039"/>
+</node>
+</node>
+</node>
+<node TEXT="Platformer" POSITION="right" ID="ID_267412834" CREATED="1476101378179" MODIFIED="1476101452320">
+<edge COLOR="#007c7c"/>
+<node TEXT="You are a ball of yarn" ID="ID_960493930" CREATED="1476101390599" MODIFIED="1476101400495"/>
+<node TEXT="Last ball of a dye lot. Must make it into someone&apos;s bag who is leaving the store or remain in the clearance bin." ID="ID_525432182" CREATED="1476101401896" MODIFIED="1476101544927"/>
+<node TEXT="Once out of the store, you must keep hidden so that you don&apos;t get returned to the store" ID="ID_1246447686" CREATED="1476101490338" MODIFIED="1476101511983">
+<node TEXT="Think search lights/hiding" ID="ID_878434899" CREATED="1476101511990" MODIFIED="1476101566086"/>
+<node TEXT="Hands rummaging around can&apos;t find you either" ID="ID_1356578731" CREATED="1476101568273" MODIFIED="1476101743388"/>
+</node>
+</node>
+<node TEXT="matching/puzzler game" POSITION="left" ID="ID_1189071004" CREATED="1476102002551" MODIFIED="1476102033808">
+<edge COLOR="#7c7c00"/>
+<node TEXT="you must keep the stash from piling up too high or it comes to life and eats you. (turns into a yarn mustache mouth and eats the screen or something. Maniacal laughter. &quot;You will never find the right yarn again!&quot;)" ID="ID_302400390" CREATED="1476102036783" MODIFIED="1476102209784"/>
+<node TEXT="You are matching dye lots of yarn" ID="ID_1664319367" CREATED="1476102210879" MODIFIED="1476102243049"/>
+<node TEXT="singlets occur, which is where 1 ball projects come in" ID="ID_1155731287" CREATED="1476102243716" MODIFIED="1476102263214"/>
+<node TEXT="3 match? or Retrieving the right yarn for a project? both could work" ID="ID_491694384" CREATED="1476102283285" MODIFIED="1476102329436"/>
 </node>
 </node>
 </map>
